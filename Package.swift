4e82363da302d56fdde76b17e1de749069cb9b6c
Daniel <daniel.zanchi@spacemushrooms.it>
2025-01-02 16:49:40 +0100

    build for macos13

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,9 +6,7 @@ import PackageDescription
 let package = Package(
   name: "SuperwallKit",
   platforms: [
-    .iOS(.v13),
-    .macOS(.v10_12),
-    .watchOS("6.2")
+    .macOS(.v13)
   ],
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -18,7 +16,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/superwall/Superscript-iOS", .exact("0.1.16"))
+    .package(url: "https://github.com/DanielZanchi/Superscript-iOS", .exact("1.0.0"))
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
